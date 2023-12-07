e318dede8e862782d234008cf6d12033439aecbc
natxofp <ignasi.fernandez@chalmers.se>
2023-12-07 23:46:49 +0100

    sad

--- src/assets/projectImages/index.js
+++ src/assets/projectImages/index.js
@@ -1,7 +1,5 @@
 import sensitProject from "./sensit-project.jpg";
-import sensit2Project from "./sensit2-project.jpg"
+import sensit2Project from "./sensit2-project.jpg";
+import cyberbridge from "./cyberbridge.png";
 
-export {
-    sensitProject,
-    sensit2Project
-}
+export { sensitProject, sensit2Project, cyberbridge };
--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,28 +1,47 @@
 export const projectsR = [
   {
-    acronym: "SensIT 2.0",
-    name: "Verifiering och prognostisering av tekniska funktionskrav på tunnelinfattning av betong - sensorbaseras prognosmetod med artificiell intelligens",
+    acronym: "PostDOFS",
+    name: "Fiberarmerade efterspända balkar enligt nya EC",
     mainApplicant: "Rasmus Rempling",
-    coApplicants: [
-      "Ignasi Fernandez",
-      "Carlos Gil Berrocal, Anders Ansell",
-      "Andreas Sjölander",
-    ],
-    leadResearcher: "August Jansson",
+    coApplicants: ["Carlos Gil Berrocal", "Rasmus Rempling"],
+    leadResearcher: "Ignasi Fernandez",
     yearStart: 2022,
     yearEnd: "ongoing",
-    founder: "Trafikverket",
-    mainInstitution: "Chalmers",
-    partners: ["Kungliga Tekniska Högskola"],
-    summary: `Projektets syfte är att bidra till framtagandet av metoder för 
-        informationsinhämtning via sensorer som kan användas för att
-        prognostisera och verifiera tekniska funktionskrav med hjälp av Artificiell intelligens.
-        Utgångspunkten och bakgrund är tunnelinfattningar av fiberarmerad sprutbetong. 
-        Konstruktionen utgörs av ett komplext material och det saknas ofta detaljerad 
-        information om kringliggande berg. Materialet och berget är i samverkan det 
-        bärande systemet som sammantaget ger en osäkerhet om dess funktion.`,
-    image: "sensit2Project",
-    link: "https://research.chalmers.se/en/project/10422",
+    founder: "SBUF",
+    mainInstitution: "NCC/Chalmers",
+    partners: ["NCC", "Thomas Betong", "Brosamverkan", "WSP"],
+    summary: `Fiberarmering och efterspänning är väletablerade tekniker inom byggbranschen; de kombineras dock sällan, 
+    troligtvis mest beroende på att fiberarmering inte är inkluderat i gällande Europeisk standard (EuroCode). Under det 
+    senaste decenniet har förberedande arbete gjorts inom CEN (CEN ansvarar för EuroCode) för att fiberarmering ska inkluderas 
+    i nästa generation av EuroCode. Nästa generation av EuroCode kommer att börja gälla vid årsskiftet 2022/2023, vilket leder 
+    till en ökad användning av teknikerna i kombination.
+    Det råder en brist på erfarenheter och kunskap av fiberarmering (FRC) och efterspänning i kombination. Beräkningar baserade 
+    på konstruktionernas bärförmåga och materialtyper finns men ytterligare utvärdering av fiberns kapacitet tillsammans med 
+    efterspänning är viktig för att entreprenören ska kunna göra produktivitets och materialvinster. Att kombinera FRC och 
+    efterspänning tros ge betydande förbättringar i byggtider, minskning av betongvolymer, lägre byggkostnader och ökad 
+    hållbarhet över hela bygglivscykeln och värdekedjan. Det kan också leda till förbättringar av hälsa och säkerhet i 
+    branschen genom att minska behovet av traditionella arbetsintensiva processer, såsom hanteringen av traditionell armering.`,
+    image: "sensitProject",
+    link: "https://research.chalmers.se/en/project/11320",
+  },
+  {
+    acronym: "Crack RASS",
+    name: "Methods for cracking risk assessment and limitation in hardening concrete",
+    mainApplicant: "Rasmus Rempling",
+    coApplicants: [
+      "Carlos Gil Berrocal",
+      "Rasmus Rempling",
+      "Ignasi Fernandez",
+    ],
+    leadResearcher: "Carlos Gil Berrocal",
+    yearStart: 2022,
+    yearEnd: "2023",
+    founder: "SBUF",
+    mainInstitution: "NCC/Chalmers",
+    partners: ["NCC", "Thomas Betong"],
+    summary: `Since the early 1990s, several SBUF projects have been carried out with a focus on concrete structures during curing and the risk of thermal cracking. Computer programs and aids/tools have also been developed to assess the stress and cracking risk of concrete structures during curing. However, no systematic review of the state of knowledge (State-Of-The-Art) has been conducted in the last 20 years; the most recent one was carried out within the SBUF and Brite/EuRam-funded project IPACS. A State-Of-The-Art report was published in 2011 in Norway, and the Norwegian Concrete Association published a guide in 2021. In Denmark, temperature cracks were investigated within the "HETEK" project in the late 90s, and testing methods and material models were presented. The primary purpose of the project is to, through an environmental analysis of methods and tools used, determine the nature of concrete during curing. The analysis will lead to a report highlighting Swedish needs for research and development of methods and tools. Increased knowledge of hardening concrete's cracking risk-related properties is of the utmost importance for the construction industry as the need for understanding and the robustness of the industry's approach and procedure has increased significantly recently.`,
+    image: "sensitProject",
+    link: "https://research.chalmers.se/en/project/11317",
   },
   {
     acronym: "SensIT Linda",
@@ -58,6 +77,46 @@ export const projectsR = [
     link: "https://research.chalmers.se/en/project/10337",
   },
   {
+    acronym: "SensIT 2.0",
+    name: "Verifiering och prognostisering av tekniska funktionskrav på tunnelinfattning av betong - sensorbaseras prognosmetod med artificiell intelligens",
+    mainApplicant: "Rasmus Rempling",
+    coApplicants: [
+      "Ignasi Fernandez",
+      "Carlos Gil Berrocal, Anders Ansell",
+      "Andreas Sjölander",
+    ],
+    leadResearcher: "August Jansson",
+    yearStart: 2022,
+    yearEnd: "ongoing",
+    founder: "Trafikverket",
+    mainInstitution: "Chalmers",
+    partners: ["Kungliga Tekniska Högskola"],
+    summary: `Projektets syfte är att bidra till framtagandet av metoder för 
+        informationsinhämtning via sensorer som kan användas för att
+        prognostisera och verifiera tekniska funktionskrav med hjälp av Artificiell intelligens.
+        Utgångspunkten och bakgrund är tunnelinfattningar av fiberarmerad sprutbetong. 
+        Konstruktionen utgörs av ett komplext material och det saknas ofta detaljerad 
+        information om kringliggande berg. Materialet och berget är i samverkan det 
+        bärande systemet som sammantaget ger en osäkerhet om dess funktion.`,
+    image: "sensit2Project",
+    link: "https://research.chalmers.se/en/project/10422",
+  },
+  {
+    acronym: "SensIT AR",
+    name: "Collaborative Augmented Reality for increased efficiency and interactivity of infrastructure maintenance and inspection",
+    mainApplicant: "Rasmus Rempling",
+    coApplicants: ["Ignasi Fernandez", "Carlos Gil Berrocal, Anders Ansell"],
+    leadResearcher: "Carlos Gil",
+    yearStart: 2019,
+    yearEnd: "2021",
+    founder: "Formas",
+    mainInstitution: "Chalmers",
+    partners: ["NCC", "WSP"],
+    summary: `Today, inspections of transport infrastructure and maintenance operations rely to a large extent on manual, analogue and fragmented processes which consequently make the current approach time consuming, inefficient and costly. A digital upgrade of the inspection protocols, methods, and workflows, which have not changed for decades, is urgently needed to increase the efficiency of on-site structural condition assessment. Together with industrial partners, Chalmers University is tackling this challenge by utilising the latest advancements in sensing technology together with the recent developments within the field of ICT. As part of the Chalmers research concept “Sensor driven cloud-based strategies for infrastructure management (SensIT)” we aim at developing a collaborative Augmented Reality (AR) tool for infrastructure inspection and maintenance. The tool will include functionalities to retrieve, analyse and visualise infrastructure data, as well as functionalities for on-site guided inspection and annotation, thereby greatly simplifying and optimizing the inspection process. The project will focus on the needs of end-users such as consultancies and the Swedish Transport Administration by actively engaging our partners in the design, evaluation and on-site testing of the AR tool. We believe that the here proposed integration of AR tools within a structural health monitoring framework has the potential to radically change the way inspections are done today. The digitisation of the entire information flow - BIM/GIS data, sensor data, in-situ simulation results, annotation, inspection history - will greatly reduce the time and costs of inspection processes and will guarantee a simplified and transparent workflow that even non-expert operators will be able to use.`,
+    image: "sensit2Project",
+    link: "https://research.chalmers.se/en/project/9273",
+  },
+  {
     acronym: "SensIT",
     name: "Sensor-Driven Cloud-Based Strategies for Infrastructure Management",
     mainApplicant: "Rasmus Rempling",
@@ -87,7 +146,7 @@ export const projectsR = [
 
 export const projectsE = [
   {
-    acronym: "SensIT AR",
+    acronym: "SensIT-AR KT",
     name: "Making the invisible visible",
     mainApplicant: "Ignasi Fernandez",
     coApplicants: ["Rasmus Rempling", "Carlos Gil Berrocal", "Mikael Joansson"],
@@ -110,64 +169,18 @@ export const projectsE = [
         (e) The use of Augmented Reality for efficient maintenance 
         inspection of structures.`,
     image: "sensitProject",
-    link: "https://research.chalmers.se/en/project/8295",
+    link: "https://intranet.chalmers.se/aktuellt/nyheter/digitaliseringssatsningen-gjorde-det-osynliga-synligt/",
   },
   {
-    acronym: "SensIT 2.0dd",
-    name: "Verifiering och prognostisering av tekniska funktionskrav på tunnelinfattning av betong - sensorbaseras prognosmetod med artificiell intelligens",
-    mainApplicant: "Rasmus Rempling",
-    coApplicants: [
-      "Ignasi Fernandez",
-      "Carlos Gil Berrocal, Anders Ansell",
-      "Andreas Sjölander",
-    ],
-    leadResearcher: "August Jansson",
-    yearStart: 2022,
-    yearEnd: "ongoing",
-    founder: "Trafikverket",
+    acronym: "Cyberbridge",
+    name: "An interactive tool to promote active learning in structural engineering courses",
+    mainApplicant: ["Ignasi Fernandez", "Carlos Gil Berrocal"],
+    leadResearcher: "Carlos Gil Berrocal",
+    yearStart: 2018,
+    yearEnd: "2019",
     mainInstitution: "Chalmers",
-    partners: ["Kungliga Tekniska Högskola"],
-    summary: `Projektets syfte är att bidra till framtagandet av metoder för 
-        informationsinhämtning via sensorer som kan användas för att
-        prognostisera och verifiera tekniska funktionskrav med hjälp av Artificiell intelligens.
-        Utgångspunkten och bakgrund är tunnelinfattningar av fiberarmerad sprutbetong. 
-        Konstruktionen utgörs av ett komplext material och det saknas ofta detaljerad 
-        information om kringliggande berg. Materialet och berget är i samverkan det 
-        bärande systemet som sammantaget ger en osäkerhet om dess funktion.`,
-    image: "sensit2Project",
+    summary: `Understanding basic concepts and assumptions is a crucial step in the learning process of any subject, yet today it is not always achieved. A change of the current teaching method is suggested in this paper through the implementation of a new learning activity based on digital tools. The use of interactive tools has been shown to be an effective method in the field of medicine. Although less frequent, interactive tools could also be beneficial in the field of civil engineering, particularly in structural engineering, where real lab experiments are large, costly and time consuming. By building a small-scale interactive bridge model, the students can have access to a live experiment in the classroom, which enables them to put in practice the concepts learnt during the lectures. This tool seeks promoting a deeper understanding of the subject, thereby increasing the ability of the students to describe the different concepts and explain the physical meaning behind the analytical equations taught in structural engineering courses.`,
+    image: "cyberbridge",
     link: "https://research.chalmers.se/en/project/10422",
-  },
-  {
-    acronym: "SensIT Lindadd",
-    name: "Shedding Light on Internal Damage: fibre optic-driven condition assessment of corroded concrete structures",
-    mainApplicant: "Ignasi Fernandez",
-    coApplicants: ["Carlos Gil Berrocal", "Rasmus Rempling"],
-    leadResearcher: "David Dackman",
-    yearStart: 2022,
-    yearEnd: "ongoing",
-    founder: "Formas",
-    mainInstitution: "Chalmers",
-    partners: ["WSP"],
-    summary: `Ensuring the integrity and serviceability of our transport infrastructure 
-        is vital for the development and welfare of society. Consequently, guaranteeing 
-        the serviceability of our infrastructure by condition assessment and proper 
-        maintenance is imperative. Yet today, condition assessment strategies rely on 
-        labour-intensive on-site inspections, providing limited and subjective data. 
-        Particularly, the detection of corrosion processes in a structure is one of the 
-        degradation mechanisms where largest uncertainties exist. In this project, 
-        we will develop novel sensor-driven supported strategies based on distributed 
-        optical fiber sensing (DOFS) to promote a digital upgrade of the current condition 
-        assessment methods. By combining experimental data from cutting-edge DOFS and the 
-        latest advancements in modelling techniques, we will provide autonomous real-time 
-        damage detection through thorough and accurate descriptions of the internal damage 
-        due to corrosion. This information is the backbone for the current state-of-the-art 
-        assessment models. This will lead to a better planning of resources, the possibility 
-        to study optimal repair solutions and the application of timely preventive measures. 
-        As a result, the service-life of our infrastructure will be effectively extended 
-        while on-site inspections will be minimized, thereby enabling a better use of our 
-        limited resources while causing a lower impact to the users, with the consequent 
-        environmental and economic savings for society.`,
-    image: "sensitProject",
-    link: "https://research.chalmers.se/en/project/10337",
   },
 ];
