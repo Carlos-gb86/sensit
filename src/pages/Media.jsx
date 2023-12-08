8aad0f4220915a3bb428b1f325a8dba3501f280c
natxofp <ignasi.fernandez@chalmers.se>
2023-12-09 00:15:07 +0100

    gallery with filter working, not teh overlay now vroken

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -3,79 +3,93 @@ import SideNav from "../components/SideNav";
 import { projectsR, projectsE } from "../constants/projects";
 
 const Media = () => {
-  const initialAcronym = "SensIT";
-  const initialProject = projectsR.find(p => p.acronym === initialAcronym) 
-                     || projectsE.find(p => p.acronym === initialAcronym) 
-                     || {};
+    // Combine all projects and add 'All' category
+    const allProjects = [{ acronym: "All", name: "All Media" }, ...projectsR, ...projectsE];
 
-  const [currentAcronym, setCurrentAcronym] = useState(initialAcronym);
-  const [currentProject, setCurrentProject] = useState(initialProject);
+    // State for managing overlay for image zoom
+    const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+    const [selectedImage, setSelectedImage] = useState(null);
 
-  useEffect(() => {
-    let foundProject = projectsR.find(project => project.acronym === currentAcronym)
-                    || projectsE.find(project => project.acronym === currentAcronym);
-
-    setCurrentProject(foundProject || {});
-  }, [currentAcronym]);
-
-  const handleProjectChange = (acronym) => setCurrentAcronym(acronym);
-  
-  const [galleryItems, setGalleryItems] = useState([]);
-
-  useEffect(() => {
-    const combinedProjects = [...projectsR, ...projectsE]; // Combine both project arrays
-
-    const loadImages = async () => {
-      let items = [];
-      const imageModules = import.meta.globEager('../assets/projectImages/**/*.+(jpg|jpeg|png)');
-
-      combinedProjects.forEach(project => {
-        const projectImages = Object.entries(imageModules)
-          .filter(([path, _]) => path.includes(`/${project.acronym}/`)) // Filter by project acronym
-          .map(([path, module], index) => ({
-            id: index, // Unique ID, combining acronym and index
-            src: module.default,
-            project: project.acronym
-          }));
-
-        items = [...items, ...projectImages];
-      });
-
-      setGalleryItems(items);
+    // Handler for clicking on an image
+    const handleImageClick = (image) => {
+        setSelectedImage(image);
+        setIsOverlayVisible(true);
     };
 
-    loadImages();
-  }, []); // Empty dependency array as the projects are static
+    // Handler for closing the overlay
+    const closeOverlay = () => {
+        setIsOverlayVisible(false);
+        setSelectedImage(null);
+    };
 
-  console.log(setGalleryItems)
+    // Prevent overlay close when clicking inside
+    const handleOverlayContentClick = (e) => {
+        e.stopPropagation();
+    };
 
- return (
-    <div className="relative flex-grow sm:flex bg-primary w-full">
-      {/* Left Side Navigation */}
-      <div className="flex flex-col">
-        <SideNav
-          title="Research"
-          navList={projectsR.map((project) => project.acronym)}
-          currentType={currentAcronym}
-          onTypeChange={handleProjectChange}
-        />
-        <SideNav
-          title="Education"
-          navList={projectsE.map((project) => project.acronym)}
-          currentType={currentAcronym}
-          onTypeChange={handleProjectChange}
-        />
-      </div>
+    // State for the currently selected project acronym
+    const [currentAcronym, setCurrentAcronym] = useState("All");
 
-      {/* Right Side Content */}
-      <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-5'>
-        {galleryItems.map(({ id, src }) => (
-        <div key={id} className='shadow-md shadow-gray-600 rounded-lg overflow-hidden'>
-          <img src={src} alt='' className='rounded-md duration-200 hover:scale-105' />
+    // State for storing gallery items
+    const [galleryItems, setGalleryItems] = useState([]);
+
+    // Load images
+    useEffect(() => {
+        let items = [];
+        const imageModules = import.meta.globEager('../assets/projectImages/**/*.+(jpg|jpeg|png)');
+
+        allProjects.forEach(project => {
+            const projectImages = Object.entries(imageModules)
+                .filter(([path, _]) => path.includes(`/${project.acronym}/`))
+                .map(([path, module], index) => ({
+                    id: project.acronym + '_' + index,
+                    src: module.default,
+                    project: project.acronym
+                }));
+
+            items = [...items, ...projectImages];
+        });
+
+        setGalleryItems(items);
+    }, []);
+
+    // Filter images based on the selected project acronym
+    const filteredGalleryItems = currentAcronym === "All" 
+        ? galleryItems 
+        : galleryItems.filter(item => item.project === currentAcronym);
+
+    return (
+        <div className="relative flex-grow sm:flex bg-primary w-full">
+            {/* Left Side Navigation */}
+            <div className="flex flex-col">
+                <SideNav
+                    title="All media"
+                    navList={allProjects.map((project) => project.acronym)}
+                    currentType={currentAcronym}
+                    onTypeChange={setCurrentAcronym}
+                />
+            </div>
+
+            {/* Right Side Content */}
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 sm:px-5'>
+                {filteredGalleryItems.map(({ id, src }) => (
+                    <div key={id} className='shadow-md shadow-gray-600 rounded-lg overflow-hidden' onClick={() => handleImageClick({ id, src })}>
+                        <img src={src} alt='' className='rounded-md duration-200 hover:scale-105' />
+                    </div>
+                ))}
+            </div>
+
+            {/* Image Overlay */}
+            {isOverlayVisible && (
+                <div className="overlay" onClick={closeOverlay}>
+                    <div className="overlay-content" onClick={handleOverlayContentClick}>
+                        {selectedImage && <img src={selectedImage.src} alt="Selected" />}
+                        <button onClick={closeOverlay}>X</button>
+                    </div>
+                </div>
+            )}
         </div>
-        ))}
-      </div>
-    </div>
-  );
+    );
 };
+
 export default Media;
