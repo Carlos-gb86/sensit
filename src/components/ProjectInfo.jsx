56745e3eef4f00a550b57f8eac375663d22090aa
natxofp <ignasi.fernandez@chalmers.se>
2023-12-08 16:41:00 +0100

    dsa

--- src/components/ProjectInfo.jsx
+++ src/components/ProjectInfo.jsx
@@ -18,21 +18,32 @@ const ProjectInfo = ({ project }) => {
       <h2 className="font-poppins font-semibold text-2xl text-white ml-10 md:ml-4 mb-6">
         {project.name}
       </h2>
-
-      <div className="flex flex-col md:flex-row">
-        <div className="carousel-container">
-          <CarouselTail imageUrls={imageUrls} />
-        </div>
-        <div className="pl-10 md:pl-4 mt-4 md:mt-0 text-white flex-grow">
-          {project.acronym && (
-            <li className="pb-4">
-              <span className="font-bold">Acronym:</span> {project.acronym}
-            </li>
-          )}
-          {/* Add other project details here */}
-        </div>
-      </div>
-
+            <div className="flex flex-col md:flex-row">
+                <img className="object-cover w-full px-10 md:w-1/2 md:px-4 max-h-[420px]" src={projectImage} alt={project.acronym} />
+                
+               <ul className="pl-10 md:pl-4 mt-4 md:mt-0 text-white">
+                    {project.acronym && 
+                        <li className='pb-4'><span className="font-bold">Acronym:</span> {project.acronym}</li>}
+                    {project.mainApplicant && 
+                        <li className='pb-4'><span className="font-bold">Main Applicant:</span> {project.mainApplicant}</li>}
+                    {project.coApplicants && project.coApplicants.length > 0 && 
+                        <li className='pb-4'><span className="font-bold">Co-Applicants:</span> {project.coApplicants.join(", ")}</li>}
+                    {project.leadResearcher && 
+                        <li className='pb-4'><span className="font-bold">Lead Researcher:</span> {project.leadResearcher}</li>}
+                    {project.yearStart && 
+                        <li className='pb-4'><span className="font-bold">Start Year:</span> {project.yearStart}</li>}
+                    {project.yearEnd && 
+                        <li className='pb-4'><span className="font-bold">End Year:</span> {project.yearEnd}</li>}
+                    {project.founder && 
+                        <li className='pb-4'><span className="font-bold">Founder:</span> {project.founder}</li>}
+                    {project.mainInstitution && 
+                        <li className='pb-4'><span className="font-bold">Main Institution:</span> {project.mainInstitution}</li>}
+                    {project.partners && project.partners.length > 0 && 
+                        <li className='pb-4'><span className="font-bold">Partners:</span> {project.partners.join(", ")}</li>}
+                    {project.link && 
+                        <li><span className="font-bold">Project Page:</span> <a className="text-blue-500 hover:underline" href={project.link} target="_blank" rel="noreferrer">{project.link}</a></li>}
+                </ul>
+            </div>
       <div className="flex flex-col md:flex-row mt-4">
         <div className="flex-grow pr-4">
           <h3 className="font-semibold text-xl mb-2 text-white pl-10 md:pl-4">
