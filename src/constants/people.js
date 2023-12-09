949f9ff2adc3d31f23e2275bc73594dd2d357db8
natxofp <ignasi.fernandez@chalmers.se>
2023-12-10 00:35:25 +0100

    cxz

--- src/constants/people.js
+++ src/constants/people.js
@@ -6,7 +6,7 @@ export const users = [
     at Chalmers University of Technology. His expertise is within structural optimization and
     machine learning, where his focus has been on set based design and lean construction.`,
     image:
-      "https://chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2F2rhfadhl%2Frasmremp.jpg%3Fwidth%3D512%26height%3D512%26rnd%3D133240819679430000%26quality%3D60%26format%3Dwebp&w=640&q=75",
+      "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2F2rhfadhl%2Frasmremp.jpg%3Fwidth%3D600%26height%3D600%26rnd%3D133240819679430000%26quality%3D60%26format%3Dwebp&w=3840&q=75",
     githubUrl: "https://github.com/johndoe",
     linkedinUrl: "https://linkedin.com/in/johndoe",
     researchGateUrl: "https://www.researchgate.net/profile/John_Doe",
@@ -17,7 +17,7 @@ export const users = [
     description: `Ignasi is associate professor at the division of structural engineering 
     at Chalmers University of Technology.`,
     image:
-      "https://chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2Fzp1abckc%2Ffignasi.jpg%3Fwidth%3D512%26height%3D512%26rnd%3D133240822543830000%26quality%3D60%26format%3Dwebp&w=640&q=75",
+      "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2Fzp1abckc%2Ffignasi.jpg%3Fwidth%3D600%26height%3D600%26rnd%3D133240822543830000%26quality%3D60%26format%3Dwebp&w=3840&q=75",
     githubUrl: "https://github.com/natxofp",
     linkedinUrl: "https://www.linkedin.com/in/natxofp/",
     researchGateUrl: "https://www.researchgate.net/profile/Ignasi-Fernandez",
