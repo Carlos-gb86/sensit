31d765fcd8b06aeb5c95fa6454e9f6158e143b80
natxofp <ignasi.fernandez@chalmers.se>
2023-12-11 11:44:11 +0100

    fdsfs

--- src/constants/people.js
+++ src/constants/people.js
@@ -7,9 +7,9 @@ export const users = [
     machine learning, where his focus has been on set based design and lean construction.`,
     image:
       "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2F2rhfadhl%2Frasmremp.jpg%3Fwidth%3D600%26height%3D600%26rnd%3D133240819679430000%26quality%3D60%26format%3Dwebp&w=3840&q=75",
-    githubUrl: "https://github.com/johndoe",
-    linkedinUrl: "https://linkedin.com/in/johndoe",
-    researchGateUrl: "https://www.researchgate.net/profile/John_Doe",
+    githubUrl: "https://github.com/rasmusrempling",
+    linkedinUrl: "https://www.linkedin.com/in/rasmusrempling/ ",
+    researchGateUrl: "https://www.researchgate.net/profile/Rasmus-Rempling",
   },
   {
     name: "Ignasi Fernandez",
@@ -24,13 +24,14 @@ export const users = [
   },
   {
     name: "Carlos Gil Berrocal",
-    jobTitle: "Senior Researcher",
-    description: `Carlos is a senior researcher and concrete specialist at Thomas Concrete Group.`,
+    jobTitle: "Senior Lecturer",
+    description: `Carlos is a senior lecturer at the division of structural engineering 
+    at Chalmers University of Technology.`,
     image:
       "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2Fto4jyqtc%2Fberrocal.jpg%3Fwidth%3D512%26height%3D512%26rnd%3D133240819959730000%26quality%3D60%26format%3Dwebp&w=2048&q=75",
-    githubUrl: "https://github.com/johndoe",
-    linkedinUrl: "https://linkedin.com/in/johndoe",
-    researchGateUrl: "https://www.researchgate.net/profile/John_Doe",
+    githubUrl: "https://github.com/Carlos-gb86",
+    linkedinUrl: "https://www.linkedin.com/in/carlos-gil-berrocal-b06b4a56/",
+    researchGateUrl: "https://www.researchgate.net/profile/Carlos-G-Berrocal",
   },
   {
     name: "Johan Lagerkvist",
@@ -39,18 +40,17 @@ export const users = [
                              at Chalmers University of Technology and Trafikverket.`,
     image:
       "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2Fgdildj0e%2Fbergenda.jpg%3Fwidth%3D600%26height%3D600%26rnd%3D133260636856470000%26quality%3D60%26format%3Dwebp&w=3840&q=75",
-    githubUrl: "https://github.com/johndoe",
+    githubUrl: "https://github.com/orgs/chalmersStructuralEngineering",
     linkedinUrl: "https://www.linkedin.com/in/johan-lagerkvist-8b05285b/",
     researchGateUrl: "https://www.researchgate.net/profile/Johan-Lagerkvist-2",
   },
   {
     name: "Linda Cusumano",
     jobTitle: "Industrial PhD Student",
-    description: `Linda is the PhD student leading the research for the  dAI Design project
-                             at Chalmers University of Technology and NCC.`,
+    description: `Linda is an industrial PhD Student working for NCC. She has experience has as a structural engineer and design manager, and is now studying various AI implementations in the construction industry.`,
     image:
       "https://media.licdn.com/dms/image/C5603AQGseL5i060buQ/profile-displayphoto-shrink_400_400-alternative/0/1529166873967?e=1707350400&v=beta&t=YPCmKN5Dymjtvrz2qUFdR8v1H0pbHxZSrf9e7CogNB8",
-    githubUrl: "https://github.com/johndoe",
+    githubUrl: "https://github.com/orgs/chalmersStructuralEngineering",
     linkedinUrl: "https://www.linkedin.com/in/linda-cusumano-290980165/",
     researchGateUrl: "https://www.researchgate.net/profile/Linda_Cusumano",
   },
@@ -61,7 +61,7 @@ export const users = [
                              at Chalmers University of Technology.`,
     image:
       "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2Flx2ipfiv%2Fjaugust.jpg%3Fwidth%3D600%26height%3D600%26rnd%3D133260636502370000%26quality%3D60%26format%3Dwebp&w=3840&q=75",
-    githubUrl: "https://github.com/johndoe",
+    githubUrl: "https://github.com/orgs/chalmersStructuralEngineering",
     linkedinUrl: "https://www.linkedin.com/in/august-jansson-6b5235161/",
     researchGateUrl: "https://www.researchgate.net/profile/August-Jansson-2",
   },
@@ -72,8 +72,8 @@ export const users = [
                              at Chalmers University of Technology and WSP.`,
     image:
       "https://www.chalmers.se/_next/image/?url=https%3A%2F%2Fcms.www.chalmers.se%2FMedia%2Fld2fm0v2%2Fdavidda.jpg%3Fwidth%3D600%26height%3D600%26rnd%3D133260636571470000%26quality%3D60%26format%3Dwebp&w=3840&q=75",
-    githubUrl: "https://github.com/johndoe",
+    githubUrl: "https://github.com/daviddackman",
     linkedinUrl: "https://www.linkedin.com/in/david-dackman-76750915b/",
-    researchGateUrl: "https://www.researchgate.net/profile/August-Jansson-2",
+    researchGateUrl: "https://www.researchgate.net/profile/David-Dackman",
   },
 ];
