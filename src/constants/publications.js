d4b45fabc736ff70d7e0f1ce0034c9a0b400120f
natxofp <ignasi.fernandez@chalmers.se>
2023-12-12 14:43:51 +0100

    fixed redirection to the blog

--- src/constants/publications.js
+++ src/constants/publications.js
@@ -72,3 +72,86 @@ export const papers = [
     link: "https://research.chalmers.se/en/publication/532603/file/532603_Fulltext.pdf",
   },
 ];
+
+export const papersSTANDBro = [
+  {
+    type: "Proceedings",
+    author:
+      "Lagerkvist J, Simonsson P, Karlsson M, Rempling R, Bosch-Sijtsema P, Laedre O.",
+    year: 2021,
+    title: "Climate impact estimation – from feasibility study to handover",
+    publication:
+      "IABSE Congress, Ghent 2021: Structural Engineering for Future Societal Needs.",
+    link: "https://doi.org/10.2749/ghent.2021.0622",
+  },
+  {
+    type: "Journal",
+    author: "Antonsson F, Lindvall D, Lagerkvist J, Rempling R.",
+    year: 2021,
+    title: "Optimal time for contractors to enter infrastructure projects",
+    publication: "Procedia Computer Science",
+    link: "https://doi.org/10.1016/j.procs.2021.12.101",
+  },
+  {
+    type: "Proceedings",
+    author: "Lagerkvist J, Gil Berrocal C, Rempling R.",
+    year: 2022,
+    title:
+      "Climate smarter design of Soil-Steel Composite Bridges using Set-Based Design",
+    publication:
+      "Current Perspectives and New Directions in Mechanics, Modelling and Design of Structural Systems.",
+    link: "https://www.taylorfrancis.com/chapters/edit/10.1201/9781003348443-328/climate-smarter-design-soil-steel-composite-bridges-using-set-based-design-lagerkvist-berrocal-rempling",
+  },
+  {
+    type: "Proceedings",
+    author:
+      "Lagerkvist J, Karlsson M, Rempling R, Bosch-Sijtsema P, Laedre O, Simonsson P.",
+    year: 2022,
+    title:
+      "Important parameters for increased productivity in bridge design and production",
+    publication:
+      "IABSE Congress Nanjing 2022 - Bridges and Structures: Connection, Integration and Harmonisation",
+    link: "https://www.taylorfrancis.com/chapters/edit/10.1201/9781003348443-328/climate-smarter-design-soil-steel-composite-bridges-using-set-based-design-lagerkvist-berrocal-rempling",
+  },
+  {
+    type: "Thesis",
+    author: "Lagerkvist J.",
+    year: 2022,
+    title: "Improving productivity in design and construction of bridges",
+    publication: "Chalmers University of TEchnology. Licenciate thesis",
+    link: "https://research.chalmers.se/publication/535306/file/535306_Fulltext.pdf",
+  },
+];
+export const papersSensit2 = [
+  {
+    type: "Proceedings",
+    author: "Jansson, A., Fernandez, I., Berrocal, C. G., & Rempling, R.",
+    year: 2023,
+    title:
+      "Investigation of the Impact of Concrete Surface Treatment Methods on the Interfacial Bond Strength.",
+    publication:
+      "SynerCrete 2023: International RILEM Conference on Synergising Expertise towards Sustainability and Robustness of Cement-based Materials and Concrete Structures",
+    link: "https://doi.org/10.1007/978-3-031-33211-1_83",
+  },
+  {
+    type: "Proceedings",
+    author: "Jansson, A., Berrocal, C. G., Fernandez, I. & Rempling, R.",
+    year: 2023,
+    title:
+      "Experimental design for shotcrete tunnel lining with distributed optical fibre monitoring.",
+    publication: "Proceedings of the XXIV NCR Symposium 2022.",
+    link: "https://www.slideshare.net/richardmccarthycbise/d2-a5-august-jansson-experimental-design-for-shotcrete-tunnel-lining-with-distributed-optical-fibre-monitoringpptx",
+  },
+];
+export const papersSensitLinda = [
+  {
+    type: "Proceedings",
+    author: "Dackman, D., Fernandez, I., Gil Berrocal, C., & Rempling, R.",
+    year: 2023,
+    title:
+      "Correlations Between Localized Pitting Corrosion and Deflection in Reinforced Concrete Beams Subjected to Accelerated Corrosion",
+    publication:
+      "SynerCrete 2023: International RILEM Conference on Synergising Expertise towards Sustainability and Robustness of Cement-based Materials and Concrete Structures",
+    link: "https://doi.org/10.1007/978-3-031-33211-1_81",
+  },
+];
