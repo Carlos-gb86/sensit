a8f91167793995a2b0078d9345867e54b3d35371
natxofp <ignasi.fernandez@chalmers.se>
2023-12-14 11:53:44 +0100

    partially fixed navbar for small sizes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
         alt="sensIT"
         className="w-[124px] h-[40px] cursor-pointer"
       />
-</Link>
+      </Link>
       <ul className="list-none sm:ml-6 sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
           <li
@@ -45,6 +45,7 @@ const Navbar = () => {
             !toggle ? "hidden" : "flex"
           } z-[10] p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
+          
           <ul className="list-none flex justify-end items-start flex-1 flex-col">
             {navLinks.map((nav, index) => (
               <li
@@ -52,9 +53,11 @@ const Navbar = () => {
                 className={`font-poppins font-medium cursor-pointer text-[16px] text-dimWhite
                 ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
               >
-                <Link to={`/${nav.id}`} onClick={() => setToggle(false)}>
-                  {nav.title}
-                </Link>
+              {nav.id === 'news' 
+                ? <a href="https://blog.thesensitproject.com">{nav.title}</a>
+                :<Link to={nav.id === 'home' ? '/' : `/${nav.id}`}>
+              {nav.title}
+            </Link>}    
               </li>
             ))}
           </ul>
